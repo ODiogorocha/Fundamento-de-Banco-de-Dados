a2865a284e263389caf36908634d420af8ec6b15
GabrielMarquezan <marquezan.luz@acad.ufsm.br>
2025-05-15 20:28:07 -0300

    Fix: remodelei o BD (incompleto)

--- src/fromsoftware_db.sql
+++ src/fromsoftware_db.sql
@@ -1,57 +1,38 @@
 CREATE DATABASE IF NOT EXISTS fromsoftware_db;
 USE fromsoftware_db;
 
-CREATE TABLE empresa (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255),
-    fundacao VARCHAR(100),
-    sede VARCHAR(255),
-    industria VARCHAR(255),
-    produtos TEXT,
-    receita VARCHAR(100),
-    funcionarios VARCHAR(100)
+CREATE TABLE atividade (,
+    nome VARCHAR(255) PRIMARY KEY,
 );
 
-CREATE TABLE jogos (
+CREATE TABLE empresa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    fundacao DATE,
+    sede VARCHAR(255),
+    head VARCHAR(255),
+    FOREIGN KEY atividade REFERENCES atividade(nome)
+);
+
+CREATE TABLE plataforma (
+    idPlataforma INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    ano_lancamento YEAR,
+    FOREIGN KEY (idEmpresa) REFERENCES empresa(id)
+);
+
+CREATE TABLE produto (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255),
     ano_lancamento YEAR,
-    genero VARCHAR(100),
-    desenvolvedor_id INT,
-    FOREIGN KEY (desenvolvedor_id) REFERENCES empresa(id)
+    FOREIGN KEY idEmpresa REFERENCES empresa(id),
+    FOREIGN KEY idPlataforma REFERENCES empresa(id)
 );
 
-CREATE TABLE funcionarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255),
-    cargo VARCHAR(100),
-    data_inicio VARCHAR(100),
-    empresa_id INT,
-    FOREIGN KEY (empresa_id) REFERENCES empresa(id)
-);
+/* CHANGELOG
 
-CREATE TABLE paises (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100)
-);
-
-CREATE TABLE plataformas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100)
-);
-
-CREATE TABLE jogo_plataforma (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    jogo_id INT,
-    plataforma_id INT,
-    FOREIGN KEY (jogo_id) REFERENCES jogos(id),
-    FOREIGN KEY (plataforma_id) REFERENCES plataformas(id)
-);
-
-CREATE TABLE jogo_pais_publicacao (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    jogo_id INT,
-    pais_id INT,
-    FOREIGN KEY (jogo_id) REFERENCES jogos(id),
-    FOREIGN KEY (pais_id) REFERENCES paises(id)
-);
+15/05 - Gabriel: 
+  - remodelei o banco de dados para se encaixar com os dados fornecidos pela wikipedia
+  - criei as tabelas atividade (necessário pelo domínio flexível), empresa, plataforma e produto
+  - criei os relacionamentos entre as tabelas
+*/
