3b1fce73520ee0d1439747040025cdfd4580d349
GabrielMarquezan <marquezan.luz@acad.ufsm.br>
2025-05-30 21:27:32 -0300

    Feat: completed the webscraping. Just need to format the company data and integrate with the database

--- src/webscraper_pd.py
+++ src/webscraper_pd.py
@@ -9,6 +9,8 @@ class DataTransformer:
         content = row[3]
         regions = ['NA:', 'EU:', 'JP:', 'PAL:']
         parts = []
+
+        series_index = ['Year', 'Title', 'System', 'International publisher']
         
         for i in range(len(content)): # Pega o índice de cada caractere
             for region in regions:
@@ -27,32 +29,34 @@ class DataTransformer:
         new_lines = []
         for part in parts:
             new = pd.Series([row[0], row[1], row[2], row[3]],
-                              index=['Year', 'Title', 'System', 'International publisher'])
+                              index=series_index)
             new.iloc[3] = part
             new_lines.append(new)
 
         if parts == []:
             new = pd.Series([row[0], row[1], row[2], row[3]],
-                              index=['Year', 'Title', 'System', 'International publisher'])
+                              index=series_index)
             new_lines.append(new)  # Se não houver partes, mantém a linha original 
         
         return pd.DataFrame(new_lines)
 
 
     def __separate_system__(self, row, studio, isGamesTable=True):
-        separator = ', '
+        separator = ','
         series_index = []
-        index = 2
+        index = 2 # Índice da coluna de sistema/plataforma
+        upper_limit = 4 # Limite superior do índice para laços de repetição
         series = []
 
         # Define o index correto para a coluna de sistema/plataforma dependendo do estúdio
-        if studio == 'FromSoftware':
+        if not isGamesTable:
+            series_index = ['Year', 'Title', 'Game', 'Platform(s)']
+            index = 3
+        elif studio == 'FromSoftware':
             series_index = ['Year', 'Title', 'System', 'International publisher']
         elif studio == 'Bethesda Game Studios':
-            if isGamesTable:        
-                series_index = ['Year', 'Title', 'Platform(s)']
-            else:
-                series_index = ['Year', 'Title', 'Game', 'Platform(s)']
+            series_index = ['Year', 'Title', 'Platform(s)']
+            upper_limit = 3
 
         content = row[index]
 
@@ -66,7 +70,7 @@ class DataTransformer:
             for i in range(len(separated)):
                 parts.append(separated[i])
 
-            for i in range(0, index+2):
+            for i in range(0, upper_limit):
                 series.append(row[i]) # Cria uma lista com os valores das colunas
 
             for part in parts: # Separa os sistemas/plataformas
@@ -81,21 +85,20 @@ class DataTransformer:
     
 
     def __atomize_rows__(self, row, studio, isGamesTable=True):
-        if studio == 'FromSoftware':
+        if studio == 'FromSoftware': #Somente a FromSoftware tem 'International publisher'
             intermediate_rows = self.__separate_internacional_publisher__(row)
             final_rows = []
 
             # Concatena as linhas separadas de 'International publisher' com as 
             # linhas separadas de 'System'/'Platform(s)'
-            for row in intermediate_rows.itertuples(index=False):
-                separated = self.__separate_system__(row, studio, isGamesTable=isGamesTable)
+            for inter_row in intermediate_rows.itertuples(index=False):
+                separated = self.__separate_system__(inter_row, studio, isGamesTable=isGamesTable)
                 final_rows.append(separated)
 
             return pd.concat(final_rows, ignore_index=True)
         elif studio == 'Bethesda Game Studios':
-            new_rows = self.__separate_system__(new_rows, studio, isGamesTable=isGamesTable)
-
-        return new_rows    
+            new_rows = self.__separate_system__(row, studio, isGamesTable=isGamesTable)
+            return new_rows    
 
  
     def __get_index_from_itertuple__(self, itertuple, data):
@@ -117,35 +120,59 @@ class DataTransformer:
         else:
             raise ValueError("URL inválida. Deve ser de uma das duas empresas.")
 
+
     # separa as linhas que possuem mais de um valor nas colunas International publisher e System
     def separate_rows(self, data, url, isGamesTable=True):
+        studio = self.__get_studios__(url)
+
         for row in data.itertuples(index=False):
-            if len(row) == 4:
-                studio = self.__get_studios__(url)
+            if studio == 'Bethesda Game Studios' or studio == 'FromSoftware':
                 new_rows = self.__atomize_rows__(row, studio, isGamesTable=isGamesTable)
                 if new_rows is not None:
                     index = self.__get_index_from_itertuple__(row, data)
+                    
                     if index is not None:
                         data = data.drop(index=index, axis=0)  # remove a linha original
+                    else:
+                        print("Erro ao encontrar o índice da linha original.")
+
                     data = pd.concat([data, new_rows], axis=0) # adiciona as novas linhas formatadas
+                else :
+                    print("Erro ao separar as linhas.")
                     
-        pd.DataFrame.to_csv(data, "src\\data.csv", index=False)
-         
+        data = data.reset_index(drop=True)  # Reseta o índice do DataFrame
+        return data
+    
+
+    # Chamar antes de separar as linhas
+    def get_fromsoftware_expansion_platforms(self, expansions, games):
+        rows_list = []
+        series_index = ['Year', 'Title', 'Game', 'Platform(s)']
+        for expansion in expansions.itertuples(index=False):
+            for game in games.itertuples(index=False):
+                if expansion[2] == game[1]:
+                    new_row = pd.Series([expansion[0], expansion[1], expansion[2], game[2]],
+                                        index=series_index)
+                    rows_list.append(new_row)
+
+        return pd.DataFrame(rows_list, columns=series_index)
+            
 
 class WikipediaScraper:
-    def __init__(self):
+    def __init__(self, url):
         self.tables = None
         self.has_expansions = False
         self.read_only_infobox = True
+        self.url = url
 
 
-    def read_tables_from_wikipedia(self, url):
+    def read_tables_from_wikipedia(self):
         try:
-            self.tables = pd.read_html(url)
+            self.tables = pd.read_html(self.url)
             print(f"Número de tabelas lidas: {len(self.tables)}")
 
-            if(url == "https://en.wikipedia.org/wiki/Bethesda_Game_Studios"
-               or url == "https://en.wikipedia.org/wiki/FromSoftware"):
+            if(self.url == "https://en.wikipedia.org/wiki/Bethesda_Game_Studios"
+               or self.url == "https://en.wikipedia.org/wiki/FromSoftware"):
                 self.has_expansions = True
                 self.read_only_infobox = False            
         except Exception as e:
@@ -153,17 +180,12 @@ class WikipediaScraper:
 
 
     def extract_infobox(self):
-        data = dict()
-        infobox = self.tables[0]
-            
-        for i in range(1, len(infobox)):
-            key = infobox.iloc[i, 0] # Pega a linha i da primeira coluna
-            value = infobox.iloc[i, 1] # Pega a linha i da segunda coluna
-            data[key] = value
-            print(f"{key}: {value}")
+        if self.tables is not None:
+            return self.tables[0]
+        else:
+            print("Não foi possível extrair a infobox")
+            return None
 
-        return data
-    
 
     def extract_products(self):
         if not self.read_only_infobox:
@@ -181,11 +203,57 @@ class WikipediaScraper:
             return None
         
 
+    def formated_products_table(self):
+        if self.tables is None:
+            self.read_tables_from_wikipedia()
+
+        data = self.extract_products()
+        if data is not None:
+            transformer = DataTransformer()
+            data = transformer.separate_rows(data, self.url)
+            return data
+        else:
+            print("Não foi possível extrair a tabela de produtos.")
+            return None
+
+    
+    def formated_expansions_table(self):
+        if self.tables is None:
+            self.read_tables_from_wikipedia()
+        
+        data = self.extract_expansions()
+        if data is not None:
+            if self.url == "https://en.wikipedia.org/wiki/FromSoftware":
+                games = self.extract_products()
+                data = DataTransformer().get_fromsoftware_expansion_platforms(data, games)
+            transformer = DataTransformer()
+            data = transformer.separate_rows(data, self.url, isGamesTable=False)
+            return data
+        else:
+            print("Não foi possível extrair a tabela de expansões.")
+            return None
+
+
+    def formated_company_info(self):
+        if self.tables is None:
+            self.read_tables_from_wikipedia()
+
+        data = self.extract_infobox()
+        data.columns = [['Key', 'Value']]
+        
+        if data is not None:
+            #df = pd.DataFrame(data, columns=['Key', 'Value'])
+            data.to_csv('src\\infobox.csv', index=False)
+            return data
+        else:
+            print("Não foi possível extrair as informações da empresa.")
+            return None
+
+
 if __name__ == "__main__":
     urlFS = "https://en.wikipedia.org/wiki/FromSoftware"
     urlBTH = "https://en.wikipedia.org/wiki/Bethesda_Game_Studios"
-    scraper = WikipediaScraper()
-    scraper.read_tables_from_wikipedia(urlFS)
-    data = scraper.extract_products()
-    transformer = DataTransformer()
-    transformer.separate_rows(data, urlFS)
+    scraper = WikipediaScraper(urlFS)
+    scraper.formated_company_info()
+    #data = scraper.formated_expansions_table()
+    #data = scraper.formated_products_table()
